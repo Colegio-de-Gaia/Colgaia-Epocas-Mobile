dcb462b05a737499407f98137ad3a3e06a151b24
Miguel Ferreira <miguel.ferreira.2003@outlook.com>
2019-11-26 14:42:06 +0000

    [add] Day model

--- lib/models/OccasionModel.dart
+++ lib/models/OccasionModel.dart
@@ -1,21 +1,24 @@
 class Occasion {
+  int _id;
   String _name;
   DateTime _startAt;
   DateTime _endAt;
   DateTime _createdAt;
   DateTime _updatedAt;
 
-  Occasion(
-      this._name, this._startAt, this._endAt, this._updatedAt, this._createdAt);
+  Occasion(this._id, this._name, this._startAt, this._endAt, this._updatedAt,
+      this._createdAt);
 
   Occasion.fromJson(Map<String, dynamic> json)
-      : _name = json['name'],
+      : _id = json['id'],
+        _name = json['name'],
         _startAt = json['start_at'] as DateTime,
         _endAt = json['end_at'] as DateTime,
         _createdAt = json['created_at'] as DateTime,
         _updatedAt = json['updated_at'] as DateTime;
 
   Map<String, dynamic> toJson() => {
+        'id': _id,
         'name': _name,
         'start_at': _startAt,
         'end_at': _endAt,
@@ -23,6 +26,7 @@ class Occasion {
         'updated_at': _updatedAt
       };
 
+  int get id => _id;
   String get occasionName => _name;
   DateTime get startAt => _startAt;
   DateTime get endAt => _endAt;
